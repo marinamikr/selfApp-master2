51dee2a92adccf1a55a9ef8b883232cbf5cadba3
Marina <32869115+marinamikr@users.noreply.github.com>
2020-10-06 20:45:02 +0900

    add pod

---
+++ Pods/Target Support Files/Pods-self-selfUITests/Pods-self-selfUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_self_selfUITests : NSObject
+@end
+@implementation PodsDummy_Pods_self_selfUITests
+@end
---
+++ Pods/Target Support Files/Pods-selfTests/Pods-selfTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_selfTests : NSObject
+@end
+@implementation PodsDummy_Pods_selfTests
+@end
